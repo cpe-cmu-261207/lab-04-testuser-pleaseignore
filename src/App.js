1d1ac72798153eccd16b03f9659412995fb54808
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2021-01-11 22:04:40 +0700

    everything fine sorry for late T^T

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import { useState } from "react";
-import { CourseCard } from "./components";
+import { CourseCard } from "./components/CourseCard";
 function App() {
-  const grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F", "W"];
+  const grade = [{g:"A",v:4}, {g:"B+",v:3.5}, {g:"B",v:3}
+                 , {g:"C+",v:2.5}, {g:"C",v:2}, {g:"D+",v:1.5}
+                 , {g:"D",v:1}, {g:"F",v:0}, {g:"W",v:-1}];
   const credit = [1, 2, 3];
 
   const [myCourses, setMyCourse] = useState([]);
-  const [inputData, setInputData] = useState({});
-  const [GPA, setGPA] = useState(4.0);
+  const [inputData, setInputData] = useState({name:"",grade:"",val:0,cred:0});
+  const [GPA, setGPA] = useState(0.0);
 
   /**
    * Calculate the GPA of current courses
    * @returns the GPA of current courses
    */
-  function calculateGPA() {
+  function calculateGPA(course) {
     // TODO
+    var gpa = 0
+    var cred = 0
+    course.forEach(c => {
+      if(c.grade !== "W"){
+        gpa += Number(c.val) *Number(c.cred)
+        cred += Number(c.cred)
+      }      
+    });
+    gpa = Number(gpa)/ Number(cred)
+    if(gpa > 0.00){
+      setGPA(gpa)
+    }else{
+      setGPA(0.00)
+    }
   }
 
   /**
@@ -24,9 +40,10 @@ function App() {
   function addCourse(event) {
     event.preventDefault();
     // TODO
-
+    const course = [...myCourses,inputData]
+    setMyCourse(course)
     // recalculate GPA
-    calculateGPA();
+    calculateGPA(course);
   }
 
   /**
@@ -36,6 +53,11 @@ function App() {
    */
   function onDeleteCourse(id) {
     // TODO
+    const remain = myCourses.filter((course)=>{
+      return course.name !== id
+    })
+    setMyCourse(remain)
+    calculateGPA(remain)
   }
 
   return (
@@ -46,9 +68,37 @@ function App() {
       <div className="h-2/3 md:w-2/4 p-3 rounded-lg mx-auto overflow-auto">
         <h1 className="text-2xl my-3">My courses</h1>
         {/* TODO display courses */}
+        {myCourses.map((course)=>{
+          return (<CourseCard n= {course.name} g={course.grade} c={course.cred} del={onDeleteCourse} />)
+        })}
       </div>
       {/* TODO add course input form */}
+      <form onSubmit = {(e)=>{
+        addCourse(e)
+      }}>
+        <input  placeholder="Subject name or code " onChange ={(e)=>{
+          setInputData({...inputData,name:e.currentTarget.value})
+        }}></input>
+        <select onChange = {(e) => {
+          setInputData({...inputData,val:e.currentTarget.value,
+                        grade:e.currentTarget.options[e.currentTarget.selectedIndex].text})
+        }}>
+          <option></option>
+          {grade.map((obj)=>{
+            return (<option value={obj.v}>{obj.g}</option>)
+          })}
+        </select>
+        <select onChange ={(e) => {
+          setInputData({...inputData,cred:e.currentTarget.value})
+        }}>
+        {credit.map((obj)=>{
+            return (<option value={obj}>{obj}</option>)
+          })}
+        </select>
+        <button type="submit">add</button>
+      </form>
       {/* TODO display calculated GPA */}
+      <h1>{GPA.toFixed(2)}</h1>
     </div>
   );
 }
--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,17 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return (<>
+    <tr>
+      <td>
+      <p>วิชา : {props.n}</p>
+      <p>เกรด : {props.g}</p>
+      <p>หน่วยกิต: {props.c}</p>
+      </td>
+      <td>
+        <button onClick ={()=>{
+          props.del(props.n)
+        }}>x</button>
+      </td>
+    </tr>
+  </>);
 };
